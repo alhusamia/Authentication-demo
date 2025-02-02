b7f9a36db83cffa07b03c9555445faef364f2090
Yazan Alhusamia <yalhusamia.c@neoleap.com.sa>
2025-02-02 11:04:04 +0300

    add condition for auth using clerick

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,22 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// const isProtectedRoute = createRouteMatcher("/user-profile");
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+  // if (isProtectedRoute(req)) await auth.protect();
+  // if (!isPublicRoute(req)) await auth.protect();
+  if (!userId && !isPublicRoute) {
+    // add custom logic to run before redirecting to sign-in
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
